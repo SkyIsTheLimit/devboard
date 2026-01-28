18d832fd5a87652c09255a08e2cc11d5dcffa59f
Sandeep Prasad <sandeep3180@gmail.com>
2026-01-28 12:40:52 -0500

    Got rid of Github signin and exported base.css

--- packages/web/src/auth.ts
+++ packages/web/src/auth.ts
@@ -1,4 +1,3 @@
-import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
@@ -6,7 +5,7 @@ import { prisma } from "@/lib/prisma";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google, GitHub],
+  providers: [Google],
   pages: {
     signIn: "/signin",
   },
